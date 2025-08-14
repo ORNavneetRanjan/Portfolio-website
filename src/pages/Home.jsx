05d0cdeee9770ab68ea23311a59e60c35f5cd5e3
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2025-08-14 12:02:04 +0530

    custom styling done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ThemeToggle from "../components/ThemeToggle";
+function Home() {
+  return (
+    <>
+      <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        <h1>This is home page</h1>
+        <ThemeToggle />
+      </div>
+      {/* Theme toggle */}
+
+      {/* Background effects */}
+
+      {/* Navbar */}
+
+      {/*Main content */}
+
+      {/* Footer */}
+    </>
+  );
+}
+
+export default Home;
