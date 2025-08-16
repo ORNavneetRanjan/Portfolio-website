0c289085fa753bda1f33cd850a4f323cfc267685
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2025-08-17 02:31:23 +0530

    theme toggle done

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,15 +1,40 @@
 import { Moon, Sun } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
 function ThemeToggle() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  useEffect(() => {
+    const theme = localStorage.getItem("Theme");
+    if (theme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("Theme", "light");
+      setIsDarkMode(false);
+    }
+  });
+
   const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("Theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("Theme", "dark");
+    }
     setIsDarkMode(!isDarkMode);
   };
 
   return (
     <>
-      <button onClick={toggleTheme}>
+      <button
+        onClick={toggleTheme}
+        className={cn(
+          "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full ",
+          "transition-colors duration-300 focus:outline-hidden"
+        )}
+      >
         {isDarkMode ? (
           <Sun className="h-6 w-6 text-yellow-300" />
         ) : (
