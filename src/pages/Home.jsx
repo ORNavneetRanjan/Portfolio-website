537754245249aa78ceaa6dbf037d0f6af916b9fc
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2025-08-23 01:30:52 +0530

    background work done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import ThemeToggle from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
 function Home() {
   return (
     <>
       <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-        <h1>This is home page</h1>
         <ThemeToggle />
+
+        {/* Theme toggle */}
+        <StarBackground />
+        {/* Background effects */}
+
+        {/* Navbar */}
+
+        {/*Main content */}
+
+        {/* Footer */}
       </div>
-      {/* Theme toggle */}
-
-      {/* Background effects */}
-
-      {/* Navbar */}
-
-      {/*Main content */}
-
-      {/* Footer */}
     </>
   );
 }
