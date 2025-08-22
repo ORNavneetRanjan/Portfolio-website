f94ee3ded98dd865528b64ec35fd067c23f9e948
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2025-08-23 02:45:34 +0530

    build navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
+import Navbar from "../components/Navbar";
 function Home() {
   return (
     <>
@@ -10,7 +11,7 @@ function Home() {
         {/* Theme toggle */}
         <StarBackground />
         {/* Background effects */}
-
+        <Navbar />
         {/* Navbar */}
 
         {/*Main content */}
